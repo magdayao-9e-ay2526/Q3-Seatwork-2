0b7e0cafd30707193dccbfda57f9df8e6dac9b63
Magdayao-9E-ay2526 <magdayao.ra@obmontessori.edu.ph>
2026-02-20 11:28:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// Function for Discount Button
+function buttonDiscount (){
+    let origBalance = document.getElementById(balanceInput).value;
+    // const discountValue = .2;
+    let discountedPrice = Number(origBalance * 0.2;)
+
+    document.getElementById().innerHTML = ''
+}
